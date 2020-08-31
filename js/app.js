879ea2eb779df217c4b4a87b4a94f9c63efc2661
matmany <matmanymat@gmail.com>
2020-08-31 15:55:57 -0300

    Animaçao funcionando, falta adcionar cores as letras

---
+++ js/app.js
@@ -0,0 +1,37 @@
+let v = 1;
+function main(){
+    // let codigo1 = ["int main(){",""];
+    const codPlace = $("#codigo");
+    let codigo1 = `int main(){#print('Hey dude');`
+    let html =  generateHTML(codigo1);
+    codPlace.append(html);
+    let index = 1;
+
+   // window.setInterval(lines[index].UpdateRandomChar.bind(lines[index]), time);
+   window.setInterval(showCode2,100,index);
+
+}
+
+function generateHTML(codigo){
+    html = "";
+    oneElement = "";
+    let index = 0;
+    for (charac of codigo) {
+        if(charac === '#'){
+            html+=`<br> &nbsp;&nbsp;`
+            continue;
+        }
+        index++;
+        oneElement = `<i id="${index}" class="invible">${charac}</i>`;
+        html+= oneElement;
+    }
+
+    return html;
+}
+
+function showCode2(){
+    $(`#${v}`).removeClass("invible");
+    v++;
+}
+
+$(main);
