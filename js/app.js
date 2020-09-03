b1fe97955e79af9aaad48fd2ea6fdb732fdbb282
matmany <matmanymat@gmail.com>
2020-09-03 15:55:46 -0300

    mais linhas adcionadas na animaçao, e alinhamento dos icones ajustado

--- js/app.js
+++ js/app.js
@@ -1,47 +1,53 @@
 let v = 1;
 let interval = [];
-function main(){
+function main() {
     // let codigo1 = ["int main(){",""];
     let btn = $('#my-btn');
-    $(document).ready(function(){
+    $(document).ready(function () {
         $('.sidenav').sidenav();
-      });
-    
+    });
+
     const codPlace = $("#codigo");
-    let codigo1 = `int main(){#print('Hey dude');`
-    let html =  generateHTML(codigo1);
+    let codigo1 = `int main(){#%%printf('Fala tu');#%%return 0;#%%}` +
+        `###<?php#%%echo "<p>Olá Mundo</p>";#%%?>` +
+        `###<script>#%%alert( 'Hello, world!' );#</script>` +
+        `###namespace ConsoleApp1{##%%class Program{#%%%%static void Main(string[] args){#%%%%%%Console.WriteLine("Hello, world!");#%%%%%%Console.ReadLine();#%%%}#%%}#%}`;
+
+    let html = generateHTML(codigo1);
     codPlace.append(html);
     let index = 1;
 
-    btn.click(function(){
+    btn.click(function () {
         $('#card-fundo').addClass('dis-none');
         $('#my-nav-div').removeClass('dis-none');
         $('#resume').removeClass('dis-none');
     });
 
-   // window.setInterval(lines[index].UpdateRandomChar.bind(lines[index]), time);
-   interval = window.setInterval(showCode2,100,index);
+    interval = window.setInterval(showCode2, 100, index);
 
 }
 
-function generateHTML(codigo){
+function generateHTML(codigo) {
     html = "";
     oneElement = "";
     let index = 0;
     for (charac of codigo) {
-        if(charac === '#'){
-            html+=`<br> &nbsp;&nbsp;`
+        if (charac === '#') {
+            html += `<br> &nbsp;&nbsp;`
+            continue;
+        } else if (charac === '%') {
+            html += `&nbsp;`
             continue;
         }
         index++;
         oneElement = `<i id="${index}" class="invisible">${charac}</i>`;
-        html+= oneElement;
+        html += oneElement;
     }
 
     return html;
 }
 
-function showCode2(){
+function showCode2() {
     $(`#${v}`).removeClass("invisible");
     v++;
 }
--- test.cpp
+++ test.cpp
@@ -5,10 +5,6 @@
 //  ==========                 ==========
 
 int main(){
-    //imprime a mensagem que estive entre aspas duplas
     printf("Ola mundo.");
-
-    //valor de retorno para a função principal
-    //indicando que o programa acabou
     return 0;
 }
